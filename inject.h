ace119f7f2c4f9edc8c97bcb4370a3fc2ed69f1a
sib12004 <siena.biales@uconn.edu>
2016-07-26 11:47:35 -0400

    Removed print statements

--- inject.h
+++ inject.h
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <float.h>
 #include <time.h>
-#include <stdio.h>
 double fRand(double fMin, double fMax);
 double inject(double i);
 double error_rate = 50;
@@ -18,11 +17,9 @@ static sRandInitializer *aVar = new sRandInitializer();
 double inject(double i)
 {
   if((double)rand() / RAND_MAX * 100 <error_rate){
-      printf("Perturbed\n");
       return fRand(DBL_MIN, DBL_MAX);
     }
   else{
-    printf("Not Perturbed\n");
       return i;
       }
 }
